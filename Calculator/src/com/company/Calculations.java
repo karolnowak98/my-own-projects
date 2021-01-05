dae751342954a36f6028d80544520474be160d49
karolnowak <karol.nowak2298@gmail.com>
2021-01-05 16:19:47 +0100

    My 1 day of Calculator Project - Created simple GUI application

---
+++ Calculator/src/com/company/Calculations.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Calculations {
+    private float[] values;
+    private static int counter;
+
+
+
+
+    public static float add(float a, float b){
+        return a+b;
+    }
+
+    public static float subtract(float a, float b){
+        return a-b;
+    }
+
+    public static float multiplication(float a, float b){
+        return a*b;
+    }
+
+    public static float divide(float a, float b){
+        return a/b;
+    }
+
+
+
+}
